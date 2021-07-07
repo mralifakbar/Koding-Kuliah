6d388d8b9a8a409f6003ec5761e4fc83eef2fd89
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2021-07-07 09:33:16 +0700

    1

--- setsHR.cpp
+++ setsHR.cpp
@@ -3,7 +3,36 @@ using namespace std;
 
 int main()
 {
-    int Q;
+    int Q, y, x;
     cin >> Q;
-    
+    set <int> s;
+    set <int>::iterator it;
+    for(int i = 0; i < Q; i++)
+    {
+        cin >> y >> x;
+
+        switch (y)
+        {
+        case 1:
+            s.insert(x);
+            break;
+        
+        case 2:
+            s.erase(x);
+            break;
+        
+        case 3:
+        {
+            it = s.find(x);
+            if(it == s.end())
+            {
+                cout << "No\n";
+            }
+            else
+            {
+                cout << "Yes\n";
+            }
+        }
+        }
+    }
 }
